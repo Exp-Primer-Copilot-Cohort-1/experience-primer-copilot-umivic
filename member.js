bd8d6d9cb6756456f962751bb0ebe00671992f0b
Hao Liu <67887818+umivic@users.noreply.github.com>
2023-09-19 16:25:18 +0000

    second push

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+  let skills = [
+    { id: 1, name: 'HTML', level: 'advanced' },
+    { id: 2, name: 'CSS', level: 'advanced' },
+    { id: 3, name: 'JavaScript', level: 'advanced' },
+    { id: 4, name: 'React', level: 'advanced' },
+    { id: 5, name: 'NodeJS', level: 'advanced' },
+    { id: 6, name: 'Express', level: 'advanced' },
+    { id: 7, name: 'MongoDB', level: 'advanced' },
+    { id: 8, name: 'Python', level: 'advanced' },
+    { id: 9, name: 'Django', level: 'advanced' },
+    { id: 10, name: 'C', level: 'advanced' },
+    { id: 11, name: 'C++', level: 'advanced' },
+    { id: 12, name: 'Java', level: 'advanced' },
+    { id: 13, name: 'PHP', level: 'advanced' },
+    { id: 14, name: 'Laravel', level: 'advanced' },
+    { id: 15, name: 'SQL', level: 'advanced' },
+    { id: 16, name: 'Git', level: 'advanced' },
+    { id: 17, name: 'GitHub', level: 'advanced' },
+    { id: 18, name: 'Heroku', level: 'advanced' },
+    { id: 19, name: 'Netlify', level: 'advanced' },
+  ];
+  return skills;
+}
